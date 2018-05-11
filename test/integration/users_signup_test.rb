e60bc699a723a0c36a40fdf83b28654e737abae6
shinjaehun <gunsmoke.shin@gmail.com>
2018-05-11 07:29:11 +0000

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,7 +12,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template "users/new"
     # assert_select 'form[action="/signup"]', "div.Name"
   end
-      test "valid signup information" do
+  
+  test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name:  "Example User",
@@ -22,6 +23,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.empty?
+    # assert_not flash.empty?
+    assert is_logged_in?
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,5 @@
+ENV['RAILS_ENV'] ||= 'test'
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
@@ -7,4 +9,9 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
   include ApplicationHelper
+  
+  # Returns true if a test user is logged in.
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
